b4f4c724ddd30ea99a06afe14f2042b7ee52f652
А <alexander.v_2010@mail.ru>
2018-06-03 15:33:49 +0300

    Добавьте файлы проекта.

---
+++ LG/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LG
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.WorkField = new System.Windows.Forms.PictureBox();
+			this.StartButton = new System.Windows.Forms.Button();
+			this.PauseButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SpeedTrackBar = new System.Windows.Forms.TrackBar();
+			this.SpeedTB = new System.Windows.Forms.MaskedTextBox();
+			this.CounterPB = new System.Windows.Forms.PictureBox();
+			this.countLabel = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.WorkField)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.CounterPB)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// WorkField
+			// 
+			this.WorkField.BackColor = System.Drawing.SystemColors.Window;
+			this.WorkField.Location = new System.Drawing.Point(12, 12);
+			this.WorkField.Name = "WorkField";
+			this.WorkField.Size = new System.Drawing.Size(600, 600);
+			this.WorkField.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.WorkField.TabIndex = 0;
+			this.WorkField.TabStop = false;
+			// 
+			// StartButton
+			// 
+			this.StartButton.Location = new System.Drawing.Point(672, 12);
+			this.StartButton.Name = "StartButton";
+			this.StartButton.Size = new System.Drawing.Size(181, 36);
+			this.StartButton.TabIndex = 18;
+			this.StartButton.Text = "Start";
+			this.StartButton.UseVisualStyleBackColor = true;
+			this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+			// 
+			// PauseButton
+			// 
+			this.PauseButton.Location = new System.Drawing.Point(672, 79);
+			this.PauseButton.Name = "PauseButton";
+			this.PauseButton.Size = new System.Drawing.Size(181, 36);
+			this.PauseButton.TabIndex = 19;
+			this.PauseButton.Text = "Pause";
+			this.PauseButton.UseVisualStyleBackColor = true;
+			this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(618, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(47, 13);
+			this.label1.TabIndex = 20;
+			this.label1.Text = "Enter =>";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(626, 91);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 13);
+			this.label2.TabIndex = 21;
+			this.label2.Text = "Esc =>";
+			// 
+			// SpeedTrackBar
+			// 
+			this.SpeedTrackBar.Location = new System.Drawing.Point(621, 146);
+			this.SpeedTrackBar.Maximum = 500;
+			this.SpeedTrackBar.Minimum = 10;
+			this.SpeedTrackBar.Name = "SpeedTrackBar";
+			this.SpeedTrackBar.Size = new System.Drawing.Size(156, 45);
+			this.SpeedTrackBar.TabIndex = 22;
+			this.SpeedTrackBar.Value = 500;
+			this.SpeedTrackBar.ValueChanged += new System.EventHandler(this.SpeedTrackBar_ValueChanged);
+			this.SpeedTrackBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SpeedTrackBar_MouseDown);
+			this.SpeedTrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SpeedTrackBar_MouseUp);
+			// 
+			// SpeedTB
+			// 
+			this.SpeedTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.SpeedTB.Location = new System.Drawing.Point(783, 155);
+			this.SpeedTB.Mask = "0000";
+			this.SpeedTB.Name = "SpeedTB";
+			this.SpeedTB.Size = new System.Drawing.Size(70, 26);
+			this.SpeedTB.TabIndex = 23;
+			this.SpeedTB.Text = "200";
+			this.SpeedTB.TextChanged += new System.EventHandler(this.SpeedTB_TextChanged);
+			// 
+			// CounterPB
+			// 
+			this.CounterPB.BackColor = System.Drawing.Color.Gold;
+			this.CounterPB.Location = new System.Drawing.Point(659, 231);
+			this.CounterPB.Name = "CounterPB";
+			this.CounterPB.Size = new System.Drawing.Size(35, 35);
+			this.CounterPB.TabIndex = 24;
+			this.CounterPB.TabStop = false;
+			// 
+			// countLabel
+			// 
+			this.countLabel.AutoSize = true;
+			this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.countLabel.Location = new System.Drawing.Point(700, 246);
+			this.countLabel.Name = "countLabel";
+			this.countLabel.Size = new System.Drawing.Size(88, 20);
+			this.countLabel.TabIndex = 25;
+			this.countLabel.Text = "0 съедено";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(865, 618);
+			this.Controls.Add(this.countLabel);
+			this.Controls.Add(this.CounterPB);
+			this.Controls.Add(this.SpeedTB);
+			this.Controls.Add(this.SpeedTrackBar);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.PauseButton);
+			this.Controls.Add(this.StartButton);
+			this.Controls.Add(this.WorkField);
+			this.Name = "Form1";
+			this.Text = "Zmeyka";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.WorkField)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.CounterPB)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox WorkField;
+		private System.Windows.Forms.Button StartButton;
+		private System.Windows.Forms.Button PauseButton;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TrackBar SpeedTrackBar;
+		private System.Windows.Forms.MaskedTextBox SpeedTB;
+		private System.Windows.Forms.PictureBox CounterPB;
+		private System.Windows.Forms.Label countLabel;
+	}
+}
+
---
+++ LG/Form1.cs
@@ -0,0 +1,458 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LG
+{
+	public partial class Form1 : Form
+	{
+		Zmeyka zmeya;
+		public Form1()
+		{
+			this.KeyPreview = true;
+			InitializeComponent();
+			zmeya = new Zmeyka(WorkField, countLabel);
+			zmeya.Start(SpeedTrackBar.Value);
+		}
+
+		private void Go_top(object sender, EventArgs e)
+		{	
+			zmeya.direct = Zmeyka.Direction.TOP;
+		}
+
+		private void Go_right(object sender, EventArgs e)
+		{
+			zmeya.direct = Zmeyka.Direction.RIGHT;
+		}
+
+		private void Go_left(object sender, EventArgs e)
+		{
+			zmeya.direct = Zmeyka.Direction.LEFT;
+		}
+
+		private void Go_down(object sender, EventArgs e)
+		{
+			zmeya.direct = Zmeyka.Direction.BOT;
+		}
+
+		private void PauseButton_Click(object sender, EventArgs e)
+		{
+			zmeya.Pause();
+		}
+
+		private void StartButton_Click(object sender, EventArgs e)
+		{
+			zmeya.Start(SpeedTrackBar.Value);
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch(e.KeyCode)
+			{
+				case (Keys.A):
+					{
+						Go_left(null, null);
+						break;
+					}
+				case (Keys.S):
+					{
+						Go_down(null, null);
+						break;
+					}
+				case (Keys.D):
+					{
+						Go_right(null, null);
+						break;
+					}
+				case (Keys.W):
+					{
+						Go_top(null, null);
+						break;
+					}
+				case (Keys.Escape):
+					{
+						zmeya.Pause();
+						break;
+					}
+				case (Keys.Enter):
+					{
+						zmeya.Start(SpeedTrackBar.Value);
+						break;
+					}
+			}
+		}
+
+		private void SpeedTrackBar_ValueChanged(object sender, EventArgs e)
+		{
+			zmeya.SetInterval(SpeedTrackBar.Value);
+			SpeedTB.Text = SpeedTrackBar.Value.ToString();
+		}
+
+		private void SpeedTB_TextChanged(object sender, EventArgs e)
+		{
+			SpeedTrackBar.Value = int.Parse(SpeedTB.Text);
+		}
+
+		private void SpeedTrackBar_MouseDown(object sender, MouseEventArgs e)
+		{
+			zmeya.Pause(true);
+		}
+
+		private void SpeedTrackBar_MouseUp(object sender, MouseEventArgs e)
+		{
+			zmeya.Pause(false);
+		}
+	}
+
+	public enum Cell { NONE = 0, ZMEYA, DROP }
+
+	class Zmeyka
+	{
+		Timer workingTimer;
+
+		List<Point> zmeyaCoordinate;
+
+		PictureBox pb_handle;
+		Label label_handle;
+
+		int interval = -1;
+
+		public enum Direction { LEFT, TOP, RIGHT, BOT }
+
+		MatrixField workingMatrix;
+
+		public Direction direct = Direction.RIGHT;
+
+		public Zmeyka(PictureBox pb_handle, Label label_handle)
+		{
+			this.pb_handle = pb_handle;
+			this.label_handle = label_handle;
+		}
+
+		private void ReInit()
+		{
+			label_handle.Text = "0 съедено";
+			direct = Direction.RIGHT;
+			if (workingTimer != null && workingTimer.Enabled)
+				workingTimer.Stop();
+
+			workingMatrix = new MatrixField(15, new Size(25, 25));
+
+			zmeyaCoordinate = new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0) };
+			foreach (var item in zmeyaCoordinate)
+			{
+				workingMatrix.SetCell(item.X, item.Y, Cell.ZMEYA);
+			}
+
+			workingTimer = new Timer();
+			workingTimer.Interval = interval;
+			workingTimer.Tick += (x, y) => { Tick(); };
+		}
+
+		public void Start(int interval)
+		{
+			this.interval = interval;
+			ReInit();
+			workingTimer.Interval = interval;
+			workingTimer.Start();
+		}
+
+		public void Pause()
+		{
+			if(workingTimer.Enabled)
+			{
+				workingTimer.Stop();
+			}
+			else
+			{
+				workingTimer.Start();
+			}
+		}
+
+		public void Pause(bool stopProj)
+		{
+			if (stopProj)
+			{
+				if (workingTimer.Enabled)
+				{
+					workingTimer.Stop();
+				}
+			}
+			else
+			{
+				if (!workingTimer.Enabled)
+				{
+					workingTimer.Start();
+				}
+			}
+		}
+
+		public void SetInterval(int newInterval)
+		{
+			interval = newInterval;
+			if (workingTimer != null)
+			{
+				workingTimer.Interval = newInterval;
+			}
+		}
+
+		private void RandomDrop(int procentOfDrop)
+		{
+			int randResult = 0;
+
+			Random rand = new Random(DateTime.Now.Millisecond);
+
+			randResult = rand.Next(100);
+
+			if (randResult > procentOfDrop)
+				return;
+
+			int nextPoint = rand.Next(24 * 24 - zmeyaCoordinate.Count);
+			Point dropPoint = new Point(nextPoint / 24, nextPoint % 24);
+
+			bool great = false;
+
+			for(int x = dropPoint.X; x < 24; x++)
+			{
+				for (int y = dropPoint.Y; y < 24; y++)
+				{
+					if(workingMatrix.cells[x,y] == Cell.NONE)
+					{
+						workingMatrix.cells[x, y] = Cell.DROP;
+						workingMatrix.SetCell(x, y, Cell.DROP);
+						great = true;
+						break;
+					}
+				}
+				if (great)
+					break;
+			}
+		}
+
+		private void Tick()
+		{
+			switch(direct)
+			{
+				case (Direction.TOP):
+					{
+						if(zmeyaCoordinate.Last().Y - 1 < 0 || (workingMatrix.cells[zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y - 1] == Cell.ZMEYA))
+						{
+							workingTimer.Stop();
+							MessageBox.Show("You loose :(");
+							return;
+						}
+						else if(workingMatrix.cells[zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y - 1] == Cell.DROP)
+						{
+							zmeyaCoordinate.Add(new Point(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y - 1));
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+							label_handle.Text = (zmeyaCoordinate.Count) - 3 + " съедено";
+						}
+						else
+						{
+							workingMatrix.SetCell(zmeyaCoordinate[0].X, zmeyaCoordinate[0].Y, Cell.NONE);
+							for (int x = 0; x < zmeyaCoordinate.Count - 1; x++)
+							{
+								zmeyaCoordinate[x] = zmeyaCoordinate[x + 1];
+							}
+							zmeyaCoordinate[zmeyaCoordinate.Count - 1] = new Point(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y - 1);
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+						}
+						break;
+					}
+				case (Direction.RIGHT):
+					{
+						if (zmeyaCoordinate.Last().X + 1 > 24 || (workingMatrix.cells[zmeyaCoordinate.Last().X + 1, zmeyaCoordinate.Last().Y] == Cell.ZMEYA))
+						{
+							workingTimer.Stop();
+							MessageBox.Show("You loose :(");
+							return;
+						}
+						else if (workingMatrix.cells[zmeyaCoordinate.Last().X + 1, zmeyaCoordinate.Last().Y] == Cell.DROP)
+						{
+							zmeyaCoordinate.Add(new Point(zmeyaCoordinate.Last().X + 1, zmeyaCoordinate.Last().Y));
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+							label_handle.Text = (zmeyaCoordinate.Count) - 3 + " съедено";
+						}
+						else
+						{
+							workingMatrix.SetCell(zmeyaCoordinate[0].X, zmeyaCoordinate[0].Y, Cell.NONE);
+							for (int x = 0; x < zmeyaCoordinate.Count - 1; x++)
+							{
+								zmeyaCoordinate[x] = zmeyaCoordinate[x + 1];
+							}
+
+
+							zmeyaCoordinate[zmeyaCoordinate.Count - 1] = new Point(zmeyaCoordinate.Last().X + 1, zmeyaCoordinate.Last().Y);
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+						}
+						break;
+					}
+				case (Direction.BOT):
+					{
+						if ((zmeyaCoordinate.Last().Y + 1 > 24) || (workingMatrix.cells[zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y + 1] == Cell.ZMEYA))
+						{
+							workingTimer.Stop();
+							MessageBox.Show("You loose :(");
+							return;
+						}
+						else if (workingMatrix.cells[zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y + 1] == Cell.DROP)
+						{
+							zmeyaCoordinate.Add(new Point(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y + 1));
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+							label_handle.Text = (zmeyaCoordinate.Count) - 3 + " съедено";
+						}
+						else
+						{
+							workingMatrix.SetCell(zmeyaCoordinate[0].X, zmeyaCoordinate[0].Y, Cell.NONE);
+							for (int x = 0; x < zmeyaCoordinate.Count - 1; x++)
+							{
+								zmeyaCoordinate[x] = zmeyaCoordinate[x + 1];
+							}
+							zmeyaCoordinate[zmeyaCoordinate.Count - 1] = new Point(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y + 1);
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+						}
+						break;
+					}
+				case (Direction.LEFT):
+					{
+						if (zmeyaCoordinate.Last().X - 1 < 0 || (workingMatrix.cells[zmeyaCoordinate.Last().X - 1, zmeyaCoordinate.Last().Y] == Cell.ZMEYA))
+						{
+							workingTimer.Stop();
+							MessageBox.Show("You loose :(");
+							return;
+						}
+						else if (workingMatrix.cells[zmeyaCoordinate.Last().X - 1, zmeyaCoordinate.Last().Y] == Cell.DROP)
+						{
+							zmeyaCoordinate.Add(new Point(zmeyaCoordinate.Last().X - 1, zmeyaCoordinate.Last().Y));
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+							label_handle.Text = (zmeyaCoordinate.Count) - 3 + " съедено";
+						}
+						else
+						{
+							workingMatrix.SetCell(zmeyaCoordinate[0].X, zmeyaCoordinate[0].Y, Cell.NONE);
+							for (int x = 0; x < zmeyaCoordinate.Count - 1; x++)
+							{
+								zmeyaCoordinate[x] = zmeyaCoordinate[x + 1];
+							}
+
+							zmeyaCoordinate[zmeyaCoordinate.Count - 1] = new Point(zmeyaCoordinate.Last().X - 1, zmeyaCoordinate.Last().Y);
+							workingMatrix.SetCell(zmeyaCoordinate.Last().X, zmeyaCoordinate.Last().Y, Cell.ZMEYA);
+						}
+						break;
+					}
+			}
+			RandomDrop(10);
+			pb_handle.Image = workingMatrix.GetBitmap();
+		}
+	}
+
+	public class MatrixField
+	{
+		public Cell[,] cells;
+
+		Bitmap bitmap;
+		Graphics graphics;
+
+		int CellSize;
+
+		public MatrixField(int cellSize, Size fieldSize)
+		{
+			cells = new Cell[fieldSize.Width, fieldSize.Height];
+			CellSize = cellSize;
+			
+			bitmap = new Bitmap((cellSize * fieldSize.Width + 1 * (fieldSize.Width + 1)), (cellSize * fieldSize.Height + 1 * (fieldSize.Height + 1)));
+			graphics = Graphics.FromImage(bitmap);
+
+			//draw external fringes
+			graphics.DrawLine(new Pen(Color.Gray), new Point(0, 0), new Point(0, bitmap.Height - 1));
+			graphics.DrawLine(new Pen(Color.Gray), new Point(0, bitmap.Height - 1), new Point(bitmap.Width - 1, bitmap.Height - 1));
+			graphics.DrawLine(new Pen(Color.Gray), new Point(bitmap.Width - 1, bitmap.Height), new Point(bitmap.Width - 1, 0));
+			graphics.DrawLine(new Pen(Color.Gray), new Point(bitmap.Width - 1, 0), new Point(0, 0));
+
+			//X fringes
+			for(int x = 0; x < fieldSize.Width; x++)
+			{
+				var startX = 1 + 1 * (x) + (x + 1) * cellSize;
+				graphics.DrawLine(new Pen(Color.Gray), new Point(startX, 0), new Point(startX, bitmap.Height));
+			}
+
+			//Y fringes
+			for (int x = 0; x < fieldSize.Height; x++)
+			{
+				var startY = 1 + 1 * (x) + (x + 1) * cellSize;
+				graphics.DrawLine(new Pen(Color.Gray), new Point(0, startY), new Point(bitmap.Width, startY));
+			}
+		}
+
+		public Bitmap GetBitmap()
+		{
+			return bitmap;
+		}
+
+		//public void Click(int x_coord, int y_coord)
+		//{
+
+		//	//extern bitmap or on fringe
+		//	if (x_coord >= bitmap.Width || y_coord >= bitmap.Height || x_coord == 1 || y_coord == 1)
+		//		return;
+
+		//	//raschet x
+		//	if (x_coord % (CellSize + 1) == 0)
+		//	{
+		//		return;
+		//	}
+
+		//	int xpos = (x_coord / (CellSize + 1));
+
+		//	//raschet y
+		//	if (y_coord % (CellSize + 1) == 0)
+		//	{
+		//		return;
+		//	}
+
+		//	int ypos = (y_coord / (CellSize + 1));
+
+		//	Redraw(xpos, ypos, brushColor);
+		//}
+
+		public void SetCell(int xpos, int ypos, Cell newCell)
+		{
+			cells[xpos, ypos] = newCell;
+			Redraw(xpos, ypos, newCell);
+		}
+
+		private void Redraw(int xpos, int ypos, Cell newCell)
+		{
+			int xstart = 1 + 1 * (xpos) + (xpos) * CellSize;
+			int ystart = 1 + 1 * (ypos) + (ypos) * CellSize;
+
+			Color color = new Color();
+			switch(newCell)
+			{
+				case (Cell.DROP):
+					{
+						color = Color.Gold;
+						break;
+					}
+				case (Cell.NONE):
+					{
+						color = Color.White;
+						break;
+					}
+				case (Cell.ZMEYA):
+					{
+						color = Color.Green;
+						break;
+					}
+			}
+
+			graphics.FillRectangle(new SolidBrush(color), new Rectangle(xstart, ystart, CellSize, CellSize));
+		}
+	}
+}
